c775aea6ab82fb41c5273d016bce223397738023
chino4242 <ryanj.contino@gmail.com>
2025-11-26 07:16:53 -0500

    adds CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,24 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.api.endpoints import calculator
 
 app = FastAPI(
     title="Proving Ground Fitness API",
     description="Backend logic for fitness benchmarking and tracking.",
     version="0.1.0"
+)
+
+origins = [
+    "http://localhost:5173",  # The React App
+    "http://127.0.0.1:5173",  # Alternative localhost URL
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
 )
 
 @app.get("/")
