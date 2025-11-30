3e06fcc57e243a0102c160be6db77f7f40864b15
chino4242 <ryanj.contino@gmail.com>
2025-11-30 07:40:38 -0500

    Troubleshooting AWS

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://n3c4c8lsk5.execute-api.us-east-1.amazonaws.com/dev/api';
+const API_URL = 'https://n3c4c8lsk5.execute-api.us-east-1.amazonaws.com/dev';
 
 export interface RankResponse {
     rank_name: string;
